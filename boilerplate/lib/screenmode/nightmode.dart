308506ff068dc1f167ea8c79b2427b57f3f848b2
Roshan pcy <godxrosu@gmail.com>
2024-11-28 19:25:07 +0530

    added data

--- boilerplate/lib/screenmode/nightmode.dart
+++ boilerplate/lib/screenmode/nightmode.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 ThemeData dark_Mode = ThemeData(
     colorScheme: ColorScheme.dark(
-        surface: Colors.grey.shade900,
+        surface: const Color.fromARGB(255, 217, 127, 127),
         primary: Colors.grey.shade500,
-        secondary: Color.fromARGB(255, 85, 84, 84),
+        secondary: Colors.white,
         inversePrimary: Colors.grey.shade900));
